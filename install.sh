d66cc8df332af8af4afa4d247df3f8e502e2b937
Macky Fitz <pixelgamerx32@gmail.com>
2022-12-08 11:37:13 +0000

    Updated several files.

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 clear
 echo "Configuring REPL for NodeJS v16. Please wait..."
 #install latest node before starting (replit uses older versions...)
-npm install --save-dev node@16.9 && npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
+npm install --save-dev node@16
+npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
 clear
 echo "Preparing to install application dependencies..."
 npm clean-install --also=dev
--- main.sh
+++ main.sh
@@ -2,5 +2,7 @@ clear
 echo "This project uses NodeJS v16 so repl startup may take a while."
 #configure latest node install since replit uses older versions by default.
 npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
+node -v
+echo "Version configuration complete! Starting bot application..."
 clear
 npm start
