9ad98be5a64dc409e9105a3121e7d98d216b62b0
Macky Fitz <pixelgamerx32@gmail.com>
2022-12-08 11:02:13 +0000

    Add new branch specifically for replit processes.

--- replit.nix
+++ replit.nix
@@ -1,9 +1,6 @@
 { pkgs }: {
-	deps = [
-		pkgs.ffmpeg.bin
-  pkgs.nodejs-18_x
-        pkgs.nodePackages.typescript-language-server
-        pkgs.nodePackages.yarn
-        pkgs.replitPackages.jest
-	];
+    deps = [
+        pkgs.bashInteractive
+        pkgs.man
+    ];
 }
