e9b5a204d66a96399e90071c5286a69c7243553e
Macky Fitz <pixelgamerx32@gmail.com>
2022-12-08 11:06:53 +0000

    Updated with sh scripts for start up.

--- main.sh
+++ main.sh
@@ -1,1 +1,6 @@
-echo Hello World
+clear
+echo "This project uses NodeJS v16 so repl startup may take a while."
+#configure latest node install since replit uses older versions by default.
+npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
+clear
+npm start
